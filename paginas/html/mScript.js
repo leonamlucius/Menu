f073acd950a1d2ba680f5f391ff81e5f022e7967
Leonam L <leonam253@gmail.com>
2023-05-18 19:22:03 -0300

    Menu

---
+++ paginas/html/mScript.js
@@ -0,0 +1,9 @@
+function carregar(){
+    var qua = document.querySelector(".quadrado")
+    qua.style.display = "none"
+
+}
+function menu(){
+    var qua = document.querySelector(".quadrado")
+    qua.style.display = "block"
+}
